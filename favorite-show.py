2c72a4ae02a6a901d5adf90a3c4d2afd4d50abe0
Carlos <computerwhiz117@gmail.com>
2024-09-14 16:24:25 -0400

    added files

---
+++ favorite-show.py
@@ -0,0 +1,61 @@
+#This program will react to their person's favorite TV show and say if the show is good
+
+tv_show1 = "My Hero Academia"
+tv_show2 = "Mobile Suit Gundam"
+tv_show3= "Dragon Ball Z"
+tv_show4 = "RWBY"
+tv_show5 = "Stranger Things"
+tv_show6 = "The Walking Dead"
+tv_show7 = "Doctor Who"
+tv_show8 = "Ms. Marvel"
+tv_show9 = "She Hulk"
+tv_show10 = "Rick and Morty"
+tv_show11 = "My Adventures with Superman"
+tv_show12 = "Arcane"
+tv_show13 = "Squid Game"
+
+print("Hello, I'm going to know my thoughts on what's your favorite TV Show it can be ranged from anime to live action")
+tv_show = input("What's your favorite TV Show: ")
+
+if tv_show ==  tv_show1:
+    print("That is awesome My Hero Academia is one of the best anime ever especially with great characters with Deku and Uraraka")
+
+elif tv_show == tv_show2:
+    print("That is an awesome show Mobile Suit Gundam is one of the best mecha anime ever it has a lot of action especially with piloting the Gundam")
+
+elif tv_show == tv_show3:
+    print("That is an awesome show Dragon Ball Z is one of the best anime ever especially that Goku is an awesome character and he can summon the Dragon Balls")
+
+elif tv_show == tv_show4:
+    print("That is an awesome show RWBY has a lot of action and really great memorable characters like Ruby Rose, Yang Xiao Long, Blake Belladonna, and Weiss Schnee it is definitely a really great show")
+
+elif tv_show == tv_show5:
+    print("That is awesome Stranger Things is a great show especially that it has a group of kids trying to figure out strange events happening")
+
+elif tv_show == tv_show6:
+    print("That is awesome The Walking Dead is actually a great show especially that it's about surviving the zombie apocalypse")
+
+elif tv_show == tv_show7:
+    print("That is awesome I love Doctor Who it has the Time Lord and he travels in time and space with his companion and has a lot of great Sci-Fi stuff in that show")
+
+elif tv_show == tv_show8:
+    print("That is awesome Ms. Marvel is actually a great show especially that it's about a girl named Kamala Khan and she gets powers it's an awesome show")
+
+elif tv_show == tv_show9:
+    print("That is awesome She Hulk is one of the best Marvel shows out there especially that it's about a lawyer that gets powers becomes the She Hulk")
+
+elif tv_show == tv_show10:
+    print("That is awesome Rick and Morty is an awesome show it's about Rick Sanchez and Morty Smith that goes on advenures and it's very hilarious")
+
+elif tv_show == tv_show11:
+    print("That is awesome My Adventures with Superman is a great show especially that it's about Superman and teams up with his friends and they fight evil")
+
+elif tv_show == tv_show12:
+    print("That is an awesome show Arcane is definitely worth watching since it's based on the game League of Lengends")
+
+elif tv_show == tv_show13:
+    print("That is an awesome show it's about a group of people that has to survive from deadly games")
+
+
+else:
+    print("That is not a good TV show try watching something else")
